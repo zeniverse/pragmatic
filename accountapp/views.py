e45afb34a1b34094596b6e00c7fc090b18ead184
jeehye jang <0219alice@gmail.com>
2021-02-13 22:44:56 +0900

    Add templates directory

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'base.html')
