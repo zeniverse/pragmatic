e8988bb842f2992bc13cc4a944c25fa999b6e591
jeehye jang <0219alice@gmail.com>
2021-02-16 21:47:58 +0900

    CreateView & Sign up

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
+    # 함수형 베이스 뷰는 바로 함수 이름을 써주면 되지만, class베이스 뷰는 .as_view()를 붙여줘야한다
     path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
