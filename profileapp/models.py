891ca0dd6fdc955df48f3f276b08ae8aa94a82fc
jeehye jang <0219alice@gmail.com>
2021-02-19 22:16:14 +0900

    Migrate Profile Model & Create Profile

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,5 +9,5 @@ class Profile(models.Model):
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
-    massage = models.CharField(max_length=100, null=True)
+    message = models.CharField(max_length=100, null=True)
 
