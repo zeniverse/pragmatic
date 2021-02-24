12bd258409d9e3ce05afa06e7531e964cdaa429b
jeehye jang <0219alice@gmail.com>
2021-02-24 15:36:23 +0900

    Create CommentCreateView & FormMixin

---
+++ commentapp/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+from commentapp.models import Comment
+
+
+class CommentCreationForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+
